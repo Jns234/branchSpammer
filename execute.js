b2382b74e59eba6784509bd6bb5f8b20a1cd5eac
Jan Kängsepp <TA-19E-206@tptlive.ee>
2022-02-22 14:44:14 +0200

    1

--- execute.js
+++ execute.js
@@ -1,8 +1,43 @@
 const { exec } = require("child_process");
 
-for (let i=0 ; i<28; i++){
+for (let i=0 ; i<2; i++){
 
 exec("git checkout -b"+ i, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+exec("git add .", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("git commit -m "+'"'+i+'"', (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("git push --set-upstream origin "+i, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
